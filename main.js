8d53bee3e350d6e72d8bac35bce5a511f2c7ae5d
blaglan <blaglan@users.noreply.github.com>
2017-02-18 13:00:37 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-$(".col3").text( "The DOM is now loaded and can be manipulated.");
 $(document).ready(function(){
     $(".col4").text( "The DOM is now loaded and can be manipulated.");
     $("#single-course-select").on("click", function(){
-        $("[id='row1'][class='col1']").text("1");
+        $("[class='row1'][class='col1']").text("1");
         $(".col2").text("2");
     });
 });
