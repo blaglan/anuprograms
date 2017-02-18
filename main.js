e8e5185d7b41253374f505ad73162e92383673a9
blaglan <blaglan@users.noreply.github.com>
2017-02-18 20:37:53 +1100

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ $(document).ready(function(){
         $(".row9, .row10").hide();
     });
 });
+
+window.onload = function() {
+    var select = document.getElementById("course-select");
+    var options = ["AENGI - Bachelor of Engineering (Honours)", "2", "3", "4", "5"];
+    for (var i = 0; i < options.length; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+    }
+}
