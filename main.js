0948c9b1e1cd3e8d2b9d6eb767d856fe95e06ea5
blaglan <blaglan@users.noreply.github.com>
2017-02-18 13:20:41 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $(".col4").text( "3");
+    $(".col4").text("3");
     $("#single-course-select").on("click", function(){
-        $("div[class='row1'][class='col1']").text("1");
+        $("[.row1'][.col1]").text("1");
         $(".col2" ".col3").text("2");
     });
 });
