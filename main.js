692037220abf98fde09a423598593c4624960d03
blaglan <blaglan@users.noreply.github.com>
2017-02-18 21:36:14 +1100

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function readProgramCourses(programCode) {
     txtFile.open("GET", "/data/programs/" + programCode.toLowerCase() + ".txt", true);
     txtFile.onreadystatechange = function() {
         if (txtFile.readyState === 4) {  // document is ready to parse.
-            if (txtFile.status === 200) {  // file is found
+            if (txtFile.status === 200 || txtFile.status == 0) {  // file is found
                 allText = txtFile.responseText; 
                 lines = txtFile.responseText.split("\n");
             }
