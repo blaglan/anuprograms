47db8c0085a00a8538d42a385a950c70516291d6
blaglan <blaglan@users.noreply.github.com>
2017-02-18 21:52:00 +1100

    Update main.js

--- main.js
+++ main.js
@@ -19,12 +19,13 @@ function readProgramCourses(programCode) {
     
     var txtFile = new XMLHttpRequest();
     //txtFile.open("GET", "data/programs/" + programCode.toLowerCase() + ".txt", true);
-    txtFile.open("GET", "data/programs/aengi.txt", true);
+    //txtFile.open("GET", "data/programs/aengi.txt", true);
     txtFile.onreadystatechange = function() {
         allText = "hello?";
         if (txtFile.readyState === 4) {  // document is ready to parse.
             allText = "g'day?";
             if (txtFile.status === 200 || txtFile.status == 0) {  // file is found
+                txtFile.open("GET", "data/programs/aengi.txt", true);
                 allText = txtFile.responseText; 
                 lines = txtFile.responseText.split("\n");
             }
