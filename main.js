629052eb4f78eb5ddec01eda384a21bccfd83394
blaglan <blaglan@users.noreply.github.com>
2017-02-18 21:02:09 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-$(document).ready(function(){
-    var courseText = document.getElementByID("course-select").innerHTML;
-    //var programCode = getProgram(courseText);
-    //var programCode = "ASD";
-    //$(".row1.col0").text(programCode);
-    $(".row1.col0").text(courseText);
+/*$(document).ready(function(){
     
-    var cellArray = [[".row1.col1", ".row1.col2", ".row1.col3", ".row1.col4"], [".row2.col1", ".row2.col2", ".row2.col3", ".row2.col4"]];
-    $(".col4").text("3");
-    $("#single-course-select").on("click", function(){
-        //$(".row1.col1").text("1");
-        //$(".col2, .col3").text("2");
-        $(".row9, .row10").hide();
-        var i, j = 0;
-        for (i = 0; i < cellArray.length; i++) { 
-            for (j = 0; j < cellArray[i].length; j++) { 
-                $(cellArray[i][j]).text(cellArray[i][j]);
-            }
-        }
-    });
-});
+});*/
 
 /*function getProgram(courseText) {
     var programCode = "";
@@ -41,4 +23,24 @@ window.onload = function() {
         el.value = opt;
         select.appendChild(el);
     }
+    
+    var courseText = document.getElementByID("course-select").innerHTML;
+    //var programCode = getProgram(courseText);
+    //var programCode = "ASD";
+    //$(".row1.col0").text(programCode);
+    $(".row1.col0").text(courseText);
+    
+    var cellArray = [[".row1.col1", ".row1.col2", ".row1.col3", ".row1.col4"], [".row2.col1", ".row2.col2", ".row2.col3", ".row2.col4"]];
+    $(".col4").text("3");
+    $("#single-course-select").on("click", function(){
+        //$(".row1.col1").text("1");
+        //$(".col2, .col3").text("2");
+        $(".row9, .row10").hide();
+        var i, j = 0;
+        for (i = 0; i < cellArray.length; i++) { 
+            for (j = 0; j < cellArray[i].length; j++) { 
+                $(cellArray[i][j]).text(cellArray[i][j]);
+            }
+        }
+    });
 }
