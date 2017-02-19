ee8b598028445bbbca2bbb50b6293498875bdaf3
blaglan <blaglan@users.noreply.github.com>
2017-02-19 15:37:06 +1100

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ window.onload = function() {
         select.appendChild(element1);
     }
     
-    var cellArray = [[".row1.col1", ".row1.col2", ".row1.col3", ".row1.col4"], [".row2.col1", ".row2.col2", ".row2.col3", ".row2.col4"]];
+    var cellArray = [[".row1.col1", ".row1.col2", ".row1.col3", ".row1.col4"], [".row2.col1", ".row2.col2", ".row2.col3", ".row2.col4"], [".row3.col1", ".row3.col2", ".row3.col3", ".row3.col4"], [".row4.col1", ".row4.col2", ".row4.col3", ".row4.col4"], [".row5.col1", ".row5.col2", ".row5.col3", ".row5.col4"], [".row6.col1", ".row6.col2", ".row6.col3", ".row6.col4"], [".row7.col1", ".row7.col2", ".row7.col3", ".row7.col4"], [".row8.col1", ".row8.col2", ".row8.col3", ".row8.col4"], [".row9.col1", ".row9.col2", ".row9.col3", ".row9.col4"], [".row10.col1", ".row10.col2", ".row10.col3", ".row10.col4"]];
     $(".col4").text("3");
     $("#single-course-select").on("click", function(){
         var courseText = document.getElementById("course-select").value;
